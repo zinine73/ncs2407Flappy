8076b03e219cb30268052b2e07dad3b541371111
zinine73 <zinine@naver.com>
2024-09-20 13:17:10 +0900

    Object Pooling

--- Assets/7_Script/MovePipe.cs
+++ Assets/7_Script/MovePipe.cs
@@ -7,14 +7,17 @@ public class MovePipe : MonoBehaviour
     [SerializeField] private float speed = 0.65f;
     [SerializeField] BoxCollider2D upPipe;
     [SerializeField] BoxCollider2D downPipe;
-
+    public bool Moving {get; set;}
     void Update()
     {
         // 게임상태가 PLAY일때만 움직이도록
         if (GameManager.instance.GameState == GameManager.State.PLAY)
         {
-            // 파이프의 위치를 speed만큼 좌로 이동
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            if (Moving) // 오브젝트풀링에서 움직임 시작 조정
+            {
+                // 파이프의 위치를 speed만큼 좌로 이동
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
         }
         else if (GameManager.instance.GameState == GameManager.State.GAMEOVER)
         {
--- Assets/7_Script/PipeSpawner.cs
+++ Assets/7_Script/PipeSpawner.cs
@@ -6,8 +6,10 @@ public class PipeSpawner : MonoBehaviour
 {
     [SerializeField] private float maxTime = 1.5f; // 몇초마다 생성할건지
     [SerializeField] private float heightRange = 0.5f; // 생성 위치 y 의 랜덤 범위
-    [SerializeField] private GameObject pipePrefab; // 파이프의 프레팝 연결
-    [SerializeField] private GameObject pipePrefabRed; // 빨간 파이프 연결
+    [SerializeField] private GameObject[] pipePrefab; // 만들어 놓은 파이프 연결
+    [SerializeField] private GameObject[] pipePrefabRed; // 빨간 파이프 연결
+    private const int MAX_PIPE = 3; // 오브젝트풀링을 위해 미리 만들어 놓을 최대 파이프 갯수
+    private int pipeIndex = 0; // 오브젝트풀링을 위한 인덱스 저장용 변수
     private float timer; 
 
     void Update()
@@ -33,10 +35,26 @@ public class PipeSpawner : MonoBehaviour
         // 랜덤으로 y 값 정해서, 생성될 파이프의 위치 정하기
         Vector3 spawnPos = transform.position + new Vector3(0, Random.Range(-heightRange, heightRange));
         // 랜덤으로 녹색, 빨간색 파이프 선택
-        GameObject pipePf = (Random.Range(0, 100) < 10) ? pipePrefabRed : pipePrefab;
+        //GameObject pipePf = (Random.Range(0, 100) < 10) ? pipePrefabRed : pipePrefab;
         // Instantiate로 생성, 생성된 객체는 pipe라는 GameObject에 할당
-        GameObject pipe = Instantiate(pipePf, spawnPos, Quaternion.identity);
+        //GameObject pipe = Instantiate(pipePf, spawnPos, Quaternion.identity);
         // 5초뒤에 pipe 객체 파괴
-        Destroy(pipe, 5f);
+        //Destroy(pipe, 5f);
+
+        // 오브젝트 풀링
+        if (Random.Range(0, 100) < 10)
+        {
+            pipePrefabRed[pipeIndex].transform.SetPositionAndRotation(spawnPos, Quaternion.identity);
+            pipePrefabRed[pipeIndex].GetComponent<MovePipe>().Moving = true;
+        }
+        else
+        {
+            pipePrefab[pipeIndex].transform.SetPositionAndRotation(spawnPos, Quaternion.identity);
+            pipePrefab[pipeIndex].GetComponent<MovePipe>().Moving = true;
+        }
+        if (++pipeIndex == MAX_PIPE) // 최대파이프 갯수에 도달하면
+        {
+            pipeIndex = 0; // 인덱스를 다시 0으로 시작
+        }
     }
 }
